f688c7f18a260692f07a79fbe35c74636f115549
Adrian Radomski <radomski.adr@gmail.com>
2020-10-29 17:32:54 +0100

    placeholders

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,47 +11,47 @@
 
             <div>
                 <x-jet-label for="name" value="{{ __('Name') }}" />
-                <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-jet-input id="name" class="block mt-1 w-full" placeholder="Name & Surname" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-jet-input id="email" class="block mt-1 w-full" placeholder="E-Mail" type="email" name="email" :value="old('email')" required />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+                <x-jet-input id="password" class="block mt-1 w-full" placeholder="Password" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
                 <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-jet-input id="password_confirmation" class="block mt-1 w-full" placeholder="Repeat Password" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="work" value="{{ __('Work') }}" />
-                <x-jet-input id="work" class="block mt-1 w-full" type="text" name="work" :value="old('work')" required autofocus autocomplete="work" />
+                <x-jet-label for="work" value="{{ __('Briefly describe where you work or what you study.') }}" />
+                <x-jet-input id="work" class="block mt-1 w-full" placeholder="I study medicine and work at a hospital." type="text" name="work" :value="old('work')" required autofocus autocomplete="work" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="hobbies" value="{{ __('Hobbies') }}" />
-                <x-jet-input id="hobbies" class="block mt-1 w-full" type="text" name="hobbies" :value="old('hobbies')" required autofocus autocomplete="hobbies" />
+                <x-jet-label for="hobbies" value="{{ __('What are your interests and passions (4 hobbies)?') }}" />
+                <x-jet-input id="hobbies" class="block mt-1 w-full" placeholder="Tennis, Travel, Startups, Chess etc." type="text" name="hobbies" :value="old('hobbies')" required autofocus autocomplete="hobbies" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="plans" value="{{ __('Plans') }}" />
-                <x-jet-input id="plans" class="block mt-1 w-full" type="text" name="plans" :value="old('plans')" required autofocus autocomplete="plans" />
+                <x-jet-label for="plans" value="{{ __('How do you see yourself in 10 years?') }}" />
+                <x-jet-input id="plans" class="block mt-1 w-full" placeholder="Describe yourself in 10 years." type="text" name="plans" :value="old('plans')" required autofocus autocomplete="plans" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="why" value="{{ __('Why Us?') }}" />
-                <x-jet-input id="why" class="block mt-1 w-full" type="text" name="why" :value="old('why')" required autofocus autocomplete="why" />
+                <x-jet-label for="why" value="{{ __('Why do you need community and new connections?') }}" />
+                <x-jet-input id="why" class="block mt-1 w-full" placeholder="I want to meet new people." type="text" name="why" :value="old('why')" required autofocus autocomplete="why" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="socialmedia" value="{{ __('Social Media') }}" />
-                <x-jet-input id="socialmedia" class="block mt-1 w-full" type="text" name="socialmedia" :value="old('socialmedia')" required autofocus autocomplete="socialmedia" />
+                <x-jet-label for="socialmedia" value="{{ __('Link on social media') }}" />
+                <x-jet-input id="socialmedia" class="block mt-1 w-full" placeholder="Facebook, Instagram, LinkedIn, etc." type="text" name="socialmedia" :value="old('socialmedia')" required autofocus autocomplete="socialmedia" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
