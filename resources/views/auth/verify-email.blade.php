7d5fb424549f45b778b857ac43457f997da1aa8d
Adrian Radomski <radomski.adr@gmail.com>
2020-11-15 12:32:58 +0100

    Minor changes

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -10,7 +10,7 @@
         </div>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another. Don't forget to check your spam.') }}
         </div>
 
 
