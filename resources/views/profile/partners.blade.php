8004e3db6f48ed12755c96ed85bd4751f97d031b
artem <gazizov.elektrotechnik@gmail.com>
2020-11-22 19:03:53 +0100

    commit

--- resources/views/profile/partners.blade.php
+++ resources/views/profile/partners.blade.php
@@ -17,8 +17,9 @@
     </x-slot>
 
     @if($error == false)
-    <!--
+    
     <x-slot name="content">
+    <!--
         <h3 class="text-lg font-medium text-gray-900">
                 {{ __($data->name) }}
         </h3>
