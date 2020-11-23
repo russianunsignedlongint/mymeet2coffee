65eb63e7adba2492462bb352170cde5eed9e9dfa
artem <gazizov.elektrotechnik@gmail.com>
2020-11-23 01:56:19 +0100

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,9 +62,8 @@
         </div>
 
         <div class="flex items-center justify-center mt-4">
-            </br></br></br>
-            <x-jet-button>
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 ml-4" href="{{ route('privacy-policy') }}">
+    
+            <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('privacy-policy') }}">
                     {{ __('Privacy Policy & Imprint') }}
                 </a>
             </x-jet-button>
