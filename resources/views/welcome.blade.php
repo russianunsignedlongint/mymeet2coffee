6b7019577e5347d41757af1a8264cb78a058f02a
artem <gazizov.elektrotechnik@gmail.com>
2021-01-12 11:46:23 +0100

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <title>AI Matching</title>
+    <title>App Name - @yield('title')</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
